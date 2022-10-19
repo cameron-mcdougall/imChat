89ecede9d84b5e18cfc1d5a499c7aba282ffe4fd
Cameron McDougall <cammcd116@gmail.com>
2022-10-19 08:02:53 +0100

    heroku workaround

--- api.js
+++ api.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const socketIO = require('socket.io');
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
-app.get('/', (req, res) => {
-    res.render('index.ejs');
-});
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-io.sockets.on('connection', socket => {
+const io = socketIO(server);
+
+io.on('connection', socket => {
     socket.on('username', username => {
         socket.username = username;
         io.emit('is_online', `+ <i><strong>${socket.username}</strong> joned the chat.</i>`);
@@ -20,8 +22,4 @@ io.sockets.on('connection', socket => {
     socket.on('chat_message', message => {
         io.emit('chat_message', `<strong>${socket.username}</strong>: ${message}`);
     });
-});
-
-const server = http.listen(8080, function() {
-    console.log('listening on *:8080');
 });
